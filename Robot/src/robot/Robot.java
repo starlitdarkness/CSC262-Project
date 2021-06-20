542be36ab2ccca99f946f6ccd9f1610dc1ced58e
starlitdarkness <amber.n.mazza@gmail.com>
2021-06-20 18:31:23 -0400

    new file:   robot/Robot.class 	modified:   robot/Robot.java

--- Robot/src/robot/Robot.java
+++ Robot/src/robot/Robot.java
@@ -7,184 +7,266 @@ package robot;
 
 /**
  *
- * @author frede
+ * @author frede & amber
  */
 public class Robot {
 
-  
 
-   private int x;
+    private int x;
 
-   private int y;
+    private int y;
 
-   private char payload;
+    private char payload;
 
-  
 
-   private char grid[][];
+    private char grid[][];
 
-  
 
-   public Robot() {
+    public Robot() {
 
-       x = 0;
+        x = 0;
 
-       y = 0;
+        y = 0;
 
-       payload = ' ';
+        payload = ' ';
 
-   }
+    }
 
-  
 
-  
+    public Robot(int x, int y, char payload) {
 
-   public Robot(int x, int y, char payload) {
+        this.x = x;
 
-       this.x = x;
+        this.y = y;
 
-       this.y = y;
+        this.payload = payload;
 
-       this.payload = payload;
+    }
 
-   }
 
-  
+    public void setGrid(char grid[][]) {
 
-   public void setGrid(char grid[][])
+        this.grid = grid;
 
-   {
+    }
 
-       this.grid = grid;
 
-   }
+    public int getX() {
 
-  
+        return x;
 
-   public int getX() {
+    }
 
-       return x;
 
-   }
+    public void setX(int x) {
 
-  
+        this.x = x;
 
-   public void setX(int x) {
+    }
 
-       this.x = x;
 
-   }
+    public int getY() {
 
-  
+        return y;
 
-   public int getY() {
+    }
 
-       return y;
 
-   }
+    public void setY(int y) {
 
-  
+        this.y = y;
 
-   public void setY(int y) {
+    }
 
-       this.y = y;
 
-   }
+    public char getPayload() {
 
-  
+        return payload;
 
-   public char getPayload() {
+    }
 
-       return payload;
 
-   }
+    public void setPayload(char payload) {
 
-  
+        this.payload = payload;
 
-   public void setPayload(char payload) {
+    }
 
-       this.payload = payload;
 
-   }
+    public void print() {
 
-  
+        System.out.println("X: " + x + " Y: " + y + " Payload: " + payload + "\n");
 
-   public void print() {
+    }
 
-       System.out.println("X: " + x + " Y: " + y + " Payload: " + payload + "\n");
 
-   }
+    public boolean pickup(int lx, int ly) {
 
-  
+        if (lx != x && ly != y) {
 
-   public boolean pickup(int lx, int ly) {
+            System.out.println("Not at (" + lx + "," + ly + ")");
 
-       if(lx != x && ly != y) {
+            return false;
 
-           System.out.println("Not at (" + lx + "," + ly + ")");
+        }
 
-           return false;
 
-       }
+        char locationPayload = grid[lx][ly];
 
-      
+        if (locationPayload == ' ') {
 
-       char locationPayload = grid[lx][ly];
+            System.out.println("No load at this location");
 
-       if(locationPayload == ' ') {
+            return false;
 
-           System.out.println("No load at this location");
+        } else {
 
-           return false;
+            if (payload == ' ') {
 
-       } else {
+                payload = locationPayload;
 
-           if(payload == ' ') {
+                grid[lx][ly] = ' ';
 
-               payload = locationPayload;
+                return true;
 
-               grid[lx][ly] = ' ';
+            }
 
-               return true;
+        }
 
-           }
 
-       }
+        return false;
 
-      
+    }
 
-       return false;
 
-   }
+    public boolean dropOff(int lx, int ly) {
 
-  
+        if (lx != x && ly != y) {
 
-   public boolean dropOff(int lx, int ly) {
+            System.out.println("Not at (" + lx + "," + ly + ")");
 
-       if(lx != x && ly != y) {
+            return false;
 
-           System.out.println("Not at (" + lx + "," + ly + ")");
+        }
 
-           return false;
 
-       }
+        if (payload != ' ') {
 
-                      
+            grid[lx][ly] = payload;
 
-       if(payload != ' ') {          
+            payload = ' ';
 
-           grid[lx][ly] = payload;
+            return true;
 
-           payload = ' ';
+        }
 
-           return true;
 
-       }      
+        return false;
 
-      
+    }
 
-       return false;
 
-   }
+    // move 1 right
+    public void moveRight() {
+        if (getX() >= 25) {
+            System.out.println("Right boundary reached!");
+        }
+        else if (getX() < 25) {
+            setX(x + 1);
+        }
 
-  
+    }
+
+    // move 1 left
+    public void moveLeft() {
+        if (getX() <= 0) {
+            System.out.println("Left boundary reached!");
+        }
+        else if (getX() > 0) {
+            setX(x - 1);
+        }
+    }
+
+    // move 1 up
+    public void moveUp() {
+        if (getY() <= 0) {
+            System.out.println("Top boundary reached!");
+        }
+        else if (getY() > 0) {
+            setY(y - 1);
+        }
+    }
+
+    // move 1 down
+    public void moveDown() {
+        if (getY() >= 25) {
+            System.out.println("Bottom boundary reached!");
+        }
+        else if (getY() < 25) {
+            setY(y + 1);
+        }
+    }
+
+    public boolean moveTo(int lx, int ly) {
+        if ((lx <= 25 && lx >= 0) && (ly <= 25 && ly >= 0)) { //check to see if coordinate is within grid boundary
+
+            // change x coordinate, if needed
+            while (x != lx) {
+
+                // move right if needed
+
+                if (x < lx) {
+
+                    moveRight();
+
+                }
+
+                // move left if needed
+
+                else if (x > lx) {
+                    moveLeft();
+                }
+        }
+
+// change y coordinate
+
+        while (y != ly) {
+                // move down if needed
+                if (y < ly) {
+                    moveDown();
+                }
+// move up if needed
+                else if (y > ly) {
+                    moveUp();
+                }
+            }
+            return true;
+
+        } else
+            return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        // test program, no need to keep in future.
+Robot robot = new Robot();
+
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveDown();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveRight();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveRight();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveDown();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveRight();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveUp();
+        System.out.println(robot.getX() + ", " + robot.getY());
+robot.moveTo(7,10);
+        System.out.println(robot.getX() + ", " + robot.getY());
+
+    }
+}
